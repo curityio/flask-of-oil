f0aab5a5143d1016241b1724dc29f97a76b1102b
Nikos Anestos <nikos.anestos@curity.io>
2019-12-19 16:38:12 +0200

    updates to build with sdist

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from distutils.core import setup
 
 setup(
     name='Flask-of-Oil',
-    packages=['Flask-of-Oil'],
+    packages=['flask_of_oil'],
     version='0.0.1',
     license='Apache license 2.0',
     description='Flask OAuth Filter - an OAuth Interceptor Logic',
     author='Curity AB',
     author_email='info@curity.io',
     url='https://github.com/curity/flask-of-oil',
-    download_url='https://github.com/curity/flask-of-oil/archive/v_001.tar.gz',
     keywords=['oauth', 'flask', 'introspection', 'access token', 'jwt', 'opaque'],
     install_requires=[
         'requests',
@@ -20,7 +19,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: Apache license 2.0',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.1',
